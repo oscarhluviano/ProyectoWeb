c891c2bba54732161cc37889aed4eece1c350e15
oscarhluviano <oscarhluviano@gmail.com>
2017-05-04 08:55:05 -0500

    Se agregaron elementos materialize

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
   $(".button-collapse").sideNav();
   $(".dropdown-button").dropdown();
+  $('.carousel.carousel-slider').carousel({fullWidth: true});
+  window.setInterval(function(){$('.carousel').carousel('next')},6000)
   });
+
+twttr.widgets.createTimeline(
+  {
+    sourceType: "profile",
+    screenName: "fabric"
+  },
+  document.getElementById("tw")
+);
