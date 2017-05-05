46b956d42f3b555cedf4090b29c9f4d124649aaf
oscarhluviano <oscarhluviano@gmail.com>
2017-05-05 01:32:33 -0500

    Elementos de materialize, correciones en ubicacion de divs

--- js/app.js
+++ js/app.js
@@ -13,3 +13,11 @@ twttr.widgets.createTimeline(
   },
   document.getElementById("tw")
 );
+
+(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.9&appId=820277861463329";
+	fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
